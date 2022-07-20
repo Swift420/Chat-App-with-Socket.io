9fbdffef54ffa5ad61c788da0dd2c3439b68fd74
Swift420 <apollosdavid95@gmail.com>
2022-07-20 14:03:10 +0200

    added frontend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const { chats } = require('./data/data');
+
+const app = express();
+dotenv.config();
+
+app.get('/', function(req, res) {
+    res.send(chats)
+})
+
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, ()=> {
+    console.log(`listening on port ${PORT}`)
+})
