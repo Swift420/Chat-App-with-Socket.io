aa95f5f33b43c69ac58e949b43aab9b12c28ff79
Swift420 <apollosdavid95@gmail.com>
2022-07-20 23:09:13 +0200

    JWT

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -10,13 +10,12 @@ const userSchema = mongoose.Schema({
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
-    isAdmin: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
+    
 }, {
     timestamps: true,
 });
 
 const User = mongoose.model('User', userSchema)
+
+
+module.exports = User;
--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ connectDB()
 const app = express();
 
 
+app.use(express.json());
+
+
 
 app.use("/api/user", userRoutes)
 
