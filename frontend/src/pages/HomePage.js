94f0ab4bb73bf4f4075394ea2d5419cdb0e3ee32
Swift420 <apollosdavid95@gmail.com>
2022-07-20 16:18:49 +0200

    Tabs with Chakra

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Container, Text } from '@chakra-ui/react'
+import { Box, Container, Tab, TabList, Text, TabPanel, TabPanels, Tabs, } from '@chakra-ui/react'
 
 
 const HomePage = () => {
@@ -19,7 +19,20 @@ const HomePage = () => {
         <Text fontSize="25" fontFamily="Work sans" color="Black">Chat App</Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-        
+        <Tabs variant='soft-rounded' >
+  <TabList mb="1em">
+    <Tab width="50%">Login</Tab>
+    <Tab width="50%">Sign up</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+      {/* <Login/> */}
+    </TabPanel>
+    <TabPanel>
+       {/* <Signup/> */}
+    </TabPanel>
+  </TabPanels>
+</Tabs>
       </Box>
        
 
