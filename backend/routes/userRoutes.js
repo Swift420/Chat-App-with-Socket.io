c2371e5087b94744dc40400a8fa4ba3a78f3cd4b
Swift420 <apollosdavid95@gmail.com>
2022-07-21 16:02:20 +0200

    Auth Middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {registerUser, authUser, allUsers} = require('../controllers/userControllers')
+const {registerUser, authUser, allUsers} = require('../controllers/userControllers');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/login', authUser);
-router.get("/", allUsers)
+router.get("/", protect,allUsers)
 
 module.exports = router;
